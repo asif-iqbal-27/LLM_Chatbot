b8926ef0907a84d605628897bf82cf3cb1269675
asif-iqbal-27 <m.asifiqbal7766@gmail.com>
2025-02-08 17:32:51 +0600

    Added PDF extraction script and extracted text

---
+++ backend/extract_text.py
@@ -0,0 +1,6 @@
+from utils.pdf_processing import save_extracted_text
+
+save_extracted_text("data/english_textbook.pdf")
+save_extracted_text("data/english_teachers_guide.pdf")
+save_extracted_text("data/bangla_textbook.pdf")
+save_extracted_text("data/bangla_teachers_guide.pdf")
